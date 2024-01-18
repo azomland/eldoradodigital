3631ba2bbc5206ddcc4a9e7d3689c97a8a146480
azomland <50115272+azomland@users.noreply.github.com>
2024-01-18 01:26:42 -0300

    Update layout.tsx

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,13 +1,9 @@
-import type { Metadata } from 'next'
+
 import { Source_Code_Pro } from 'next/font/google'
 import '../styles/globals.css'
 
 const inter = Source_Code_Pro({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'El Dorado Digital',
-  description: 'Tu puerta a la riqueza inimaginable',
-}
 
 export default function RootLayout({
   children,
